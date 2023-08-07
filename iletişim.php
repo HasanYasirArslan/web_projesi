0a43e0f6113a6c779558aae6632dd0193a2a0e73
HasanYasirArslan <hasanyasirarslan@gmail.com>
2023-08-07 18:55:32 +0300

    5.commit
    İletişim sayfasına js framework ü olan vue ile denetlemeyi ekledim. Şehrim sayfasını bitirdim.

--- iletişim.php
+++ iletişim.php
@@ -5,16 +5,31 @@ echo"Doğum Tarihiniz:".$_POST["dgm"]."<br>";
 echo"Şifreniz:".$_POST["sifre"]."<br>";
 echo"Email Adresiniz:".$_POST["email"]."<br>";
 hobiYazdir();
-echo"<br>"."Takım:".$_POST["takım"]."<br>";
+echo "<br>";
+takimYazdir();
+echo "<br>";
 echo "İşletim Sisteminiz:".$_POST["sistem"]."<br>";
 echo"Tarayıcınız:".$_POST["tarayici"]."<br>";
 echo"Açıklama:".$_POST["acıklama"];
 
-function hobiYazdir()
-{
-    echo "Hobileriniz:";
+ function hobiYazdir()
+ {   
+  echo "Hobileriniz:";
+  if(isset($_POST["hobi"])){
     foreach ($_POST["hobi"] as $hobi) {
-        echo "$hobi ";
-      }
-}
+     echo "$hobi " ;
+  }
+  }
+   else     
+   echo "Herhangi bir hobi seçmediniz.";
+ }
+ function takimYazdir()
+ {   
+  echo "Takımınız:";
+  if(isset($_POST["takım"])){
+    echo "$takım " ;
+  }
+   else     
+   echo "Herhangi bir takım seçmediniz.";
+ }
 ?>
